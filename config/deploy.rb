2c35c644d53411e13a4e3f3e00a7158895e31da3
Sunny-arren <cysctoqv25lp@gmail.com>
2019-10-22 13:36:29 +0900

    carrierwave.rb の asset_host へ追加記述など

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,12 +38,13 @@ set :default_env, {
 
 # secrets.yml用のシンボリックリンクを追加
 set :linked_files, %w{ config/secrets.yml }
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-  
+
   desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
